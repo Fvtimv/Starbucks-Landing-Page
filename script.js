b54d11a4830d39705cadea1a0900f06d96c0bc75
Fvtimv <fatima.hilmi@hotmail.com>
2023-03-10 16:27:36 +0100

    Made toggle menu clickable on js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+function toggleMenu(){
+var menuToggle = document.querySelector('.toggle');
+menuToggle.classList.toggle('active')
+}
